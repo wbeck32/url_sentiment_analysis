32bbaeddcd7724b3318aaf4720f79ee7a3d5bce9
Wendy Beck <webeck@gmail.com>
2017-11-07 16:36:03 -0800

    added package.json

--- sentiment-anaysis.js
+++ sentiment-anaysis.js
@@ -1,24 +1,24 @@
-require('dotenv')
-const algorithmia = require('algorithmia')
-const input = 'https://www.metafilter.com'
-const client = algorithmia(process.env.ALGORITHMIA_KEY)
+require('dotenv');
+const algorithmia = require('algorithmia');
+const input = process.env.SITE_TO_TEST;
+const client = algorithmia(process.env.ALGORITHMIA_KEY);
 
-  client.algo('algo://web/GetLinks/0.1.5')
+client
+  .algo('algo://web/GetLinks/0.1.5')
   .pipe(input)
   .then(function(response) {
     console.log(response.get());
   });
 
-  client.algo('algo://util/Url2Text/0.1.4')
+client
+  .algo('algo://util/Url2Text/0.1.4')
   .pipe(input)
   .then(function(response) {
     console.log(response.get());
   });
 
-var input = {
-  document: 'I really like Algorithmia!'
-};
-  client.algo('algo://nlp/SentimentAnalysis/1.0.4')
+client
+  .algo('algo://nlp/SentimentAnalysis/1.0.4')
   .pipe(input)
   .then(function(response) {
     console.log(response.get());
