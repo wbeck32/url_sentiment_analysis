7d551d5540f73a56f47621b365d6550ddaea5ced
Wendy Beck <webeck@gmail.com>
2017-11-08 16:17:24 -0800

    adding async

--- sentiment-anaysis.js
+++ sentiment-anaysis.js
@@ -1,25 +1,25 @@
 require('dotenv');
 const algorithmia = require('algorithmia');
-const input = process.env.SITE_TO_TEST;
+let input = 'https://www.metafilter.com';
 const client = algorithmia(process.env.ALGORITHMIA_KEY);
 
-client
-  .algo('algo://web/GetLinks/0.1.5')
-  .pipe(input)
-  .then(function(response) {
-    console.log(response.get());
-  });
+const getLinks = async () => {
+  const links = await client.algo('algo://web/GetLinks/0.1.5').pipe(input);
+  console.log(1, links.get());
+  getText(links.get());
+};
 
-client
-  .algo('algo://util/Url2Text/0.1.4')
-  .pipe(input)
-  .then(function(response) {
-    console.log(response.get());
-  });
+const getText = async (links) => {
+  const text = await client.algo('algo://util/Url2Text/0.1.4').pipe(input)
+  siteText = {"document" : text.get()}
+  console.log(2, siteText)
+  // return text
+}
 
 client
   .algo('algo://nlp/SentimentAnalysis/1.0.4')
   .pipe(input)
   .then(function(response) {
-    console.log(response.get());
+    console.log(3, response.get());
   });
+getLinks();
