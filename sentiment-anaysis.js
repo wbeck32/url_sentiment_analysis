316a0ea52938c9496929ae178feca2773bab5f2a
Wendy Beck <webeck@gmail.com>
2017-11-08 16:28:02 -0800

    working! 😎🤓😎

--- sentiment-anaysis.js
+++ sentiment-anaysis.js
@@ -5,21 +5,18 @@ const client = algorithmia(process.env.ALGORITHMIA_KEY);
 
 const getLinks = async () => {
   const links = await client.algo('algo://web/GetLinks/0.1.5').pipe(input);
-  console.log(1, links.get());
   getText(links.get());
 };
 
-const getText = async (links) => {
-  const text = await client.algo('algo://util/Url2Text/0.1.4').pipe(input)
-  siteText = {"document" : text.get()}
-  console.log(2, siteText)
-  // return text
-}
+const getText = async links => {
+  const text = await client.algo('algo://util/Url2Text/0.1.4').pipe(input);
+  const siteText = { document: text.get() };
+  analyzeText(siteText);
+};
 
-client
-  .algo('algo://nlp/SentimentAnalysis/1.0.4')
-  .pipe(input)
-  .then(function(response) {
-    console.log(3, response.get());
-  });
+const analyzeText = async text => {
+  const sentScore = await client.algo('algo://nlp/SentimentAnalysis/1.0.4').pipe(text);
+  console.log('Sentiment score is:', sentScore.result[0].sentiment, 'on a scale from -1 to 1 with 1 being very positive.');
+};
+
 getLinks();
